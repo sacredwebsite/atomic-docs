468724ba3e80c6c105434ad805bb79f933fae2b2
Ramael Metatron <wizard@sacred.website>
2016-11-06 21:56:41 -0600

    adding components

---
+++ components/Foundation-Sites/CloseButton.php
@@ -0,0 +1,7 @@
+<!-- components/Foundation-Sites/CloseButton.php -->
+<div class="callout">
+  <button class="close-button" aria-label="Close alert" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <p>This is a static close button example.</p>
+</div>
---
+++ components/Foundation-Sites/FloatClasses.php
@@ -0,0 +1,5 @@
+<!-- components/Foundation-Sites/FloatClasses.php -->
+<div class="callout clearfix">
+  <a class="button float-left">Left</a>
+  <a class="button float-right">Right</a>
+</div>
